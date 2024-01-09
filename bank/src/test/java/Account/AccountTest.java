7b9795939e957918eb7ed26ba689105883b71e8d
quentin.robert.etu <quentin.robert.etu@univ-lille.fr>
2024-01-09 17:48:33 +0100

    3.1 Refactoring

--- bank/src/test/java/Account/AccountTest.java
+++ bank/src/test/java/Account/AccountTest.java
@@ -1,9 +1,11 @@
+package Account;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BankTest {
+public class AccountTest {
 	
 	private Account compte;
 	
