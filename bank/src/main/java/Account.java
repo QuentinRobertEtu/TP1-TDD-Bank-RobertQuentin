2005c71e7c809296cc802a7f3dd28c5697683e07
quentin.robert.etu <quentin.robert.etu@univ-lille.fr>
2024-01-09 17:45:57 +0100

    3.1 Test passe

---
+++ bank/src/main/java/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+	
+	private String owner;
+	private int debit;
+	private int credit;
+	
+	public Account(String name) {
+		this.owner = name;
+		this.credit = 0;
+		this.debit = 0;
+	}
+
+	public int getDebit() {
+		return this.debit;
+	}
+	
+	public int getCredit() {
+		return this.credit;
+	}
+}
--- bank/src/test/java/BankTest.java
+++ bank/src/test/java/BankTest.java
@@ -1,19 +1,21 @@
-class BankTest {
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankTest {
 	
 	private Account compte;
 	
 	@BeforeEach
 	public void init() {
-		this.compte = new Account();
+		this.compte = new Account("a");
 	}
 	
 	
 	@Test
 	public void TestDebitAndCreditAt0WhenAccountCreated() {
-		Account compte2 = new Account();
-		assertEquals(compte.getDebit, 0);
-		assertEquals(compte.getCredit, 0);
-		assertEquals(compte2.getDebit, 0);
-		assertEquals(compte2.getCredit, 0);
+		assertEquals(this.compte.getCredit(),0);
+		assertEquals(this.compte.getDebit(),0);
 	}
 }
