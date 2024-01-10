48a82fe6273da597ac995a752463457ba888b8b0
quentin.robert.etu <quentin.robert.etu@univ-lille.fr>
2024-01-10 12:41:02 +0100

    3.2 TDD Code

--- bank/src/main/java/Account/Account.java
+++ bank/src/main/java/Account/Account.java
@@ -2,21 +2,23 @@ package Account;
 
 public class Account {
 	
-	private String owner;
-	private float debit;
-	private float credit;
+	private double debit;
+	private double credit;
 	
 	public Account(String name) {
-		this.owner = name;
 		this.credit = 0;
 		this.debit = 0;
 	}
 
-	public float getDebit() {
+	public double getDebit() {
 		return this.debit;
 	}
 	
-	public float getCredit() {
+	public double getCredit() {
 		return this.credit;
+	}
+	
+	public void creditAccount(double d) {
+		this.credit+= d;
 	}
 }
