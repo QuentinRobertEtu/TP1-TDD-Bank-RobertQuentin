c0068868c45b277565dc9932e0443d78390275f7
quentin.robert.etu <quentin.robert.etu@univ-lille.fr>
2024-01-16 17:21:55 +0100

    4.1 TDD add credit and solde

--- bank/src/test/java/Account/SavingsAccountTest.java
+++ bank/src/test/java/Account/SavingsAccountTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Exception.CantCreditNegativeValueException;
+import Exception.CantCreditOver100000Exception;
+
 public class SavingsAccountTest{
 	
 private Account compte;
@@ -21,4 +24,17 @@ private Account compte;
 		assertEquals(this.compte.getDebit(), 0, 0);
 	}
 	
+	@Test
+	public void TestIfAccountIsCredited() throws CantCreditNegativeValueException, CantCreditOver100000Exception {
+		assertEquals(this.compte.getCredit(), 0, 0);
+		this.compte.creditAccount(500.0);
+		assertEquals(this.compte.getCredit(), 500.0, 0);
+	}
+	
+	@Test
+	public void TestIfPayIsGoodWhenCredited() throws CantCreditNegativeValueException, CantCreditOver100000Exception {
+		this.compte.creditAccount(500.0);
+		this.compte.creditAccount(500.0);
+		assertEquals(this.compte.solde(), 1000.0, 0);
+	}
 }
